b080a533b29d9848228f0bddb165d04faf1b2fa1
SMTG9092 <ysoufiane9092@gmail.com>
2026-09-14 09:15:01 +0100

    Update supabase.js

--- api/supabase.js
+++ api/supabase.js
@@ -3,21 +3,11 @@
    Supabase Configuration
    ========================================================= */
 
-/*
- * ضع هنا Project URL ديال Supabase
- *
- * مثال:
- * https://xxxxxxxxxxxx.supabase.co
- */
-const SUPABASE_URL = "https://bosliivjsmhpuietbcim.supabase.co";
+const SUPABASE_URL =
+    "https://bosliivjsmhpuietbcim.supabase.co";
 
-
-/*
- * ضع هنا Publishable / Anon Key
- *
- * ⚠️ ممنوع تحط service_role key هنا
- */
-const SUPABASE_ANON_KEY = "sb_secret_VPwl2AD03tkVc47p81p9wg_YxzYDYQ4";
+const SUPABASE_ANON_KEY =
+    "sb_publishable_dg7EBn5bAcr8VIcsbhopGA_ewJsG9iS";
 
 
 /* =========================================================
